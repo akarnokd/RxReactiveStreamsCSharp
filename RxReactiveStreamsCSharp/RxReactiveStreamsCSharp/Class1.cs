d95506ddaa18687c34372cfee54967e06a5ad42b
akarnokd <akarnokd@gmail.com>
2015-09-24 12:59:24 +0200

    Initial solution files and a few lines to see if git works.

---
+++ RxReactiveStreamsCSharp/RxReactiveStreamsCSharp/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RxReactiveStreamsCSharp
+{
+    public interface IFlowable<out T>
+    {
+
+    }
+
+    public interface ISubscriber<in T>
+    {
+
+    }
+
+    public interface ISubscription
+    {
+
+    }
+
+    public interface IProcessor<in T, out R>
+    {
+
+    }
+}
